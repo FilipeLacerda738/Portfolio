5f8ad43723416443546ddb4c63801852525e0b61
FilipeLacerda738 <jal1nff24@gmail.com>
2024-09-13 15:06:09 -0300

    EmailFuncional

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
+
+    emailjs.init("xxCmRO0tdgIOUuOIC");
+
     const form = document.getElementById("contact-form");
 
     form.addEventListener("submit", function(event) {
         event.preventDefault();
 
+              
         const name = document.getElementById("name").value;
         const email = document.getElementById("email").value;
         const message = document.getElementById("message").value;
@@ -12,11 +16,17 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log(`Email digitado: ${email}`);
         console.log(`Menssagem: ${message}`);
 
-        if (name && email && message) {
+        emailjs.send("service_euqbmgr", "template_bh7n1qv", {
+            from_name: name,
+            from_email: email,
+            message: message
+        })
+        .then(function(response) {
             alert(`Obrigado por entrar em contato, ${name}! Irei responder em breve`);
-            form.reset();  
-        } else {
-            alert("Por favor, preencha todos os campos.");
-        }
+            form.reset();     
+        }, function(error) {
+            alert("Ocorreu um erro. Tente novamente mais tarde.");
+            console.error("Erro ao enviar e-mail: ", error);
+        });
     });
 });
